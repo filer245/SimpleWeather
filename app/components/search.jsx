f2134581520edae69d6ebfb933fb9ac660944989
Sergio <69754375+filer245@users.noreply.github.com>
2024-11-29 12:45:05 +0100

    Update search.jsx

--- app/components/search.jsx
+++ app/components/search.jsx
@@ -15,8 +15,6 @@ export default function search({setter, getcity, gettemp, getclouds}) {
         getcity(data[0]);
         getclouds(data[2]);
       }catch(error){}
-      
-    }else{}
   };
 
 
